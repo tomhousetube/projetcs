93aa12e914d0403a0e041b628f82d58de3477e2b
YiChun <W.YiChun@protonmail.com>
2021-10-25 11:08:43 +0800

    first commit

---
+++ src/components/setting.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>This is a setting page</h1>
+    <p>store count is: {{ count }}</p>
+    <button @click="increment">增加1</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Setting",
+  computed: {
+    count() {
+      return this.$store.state.count;
+    },
+  },
+  // methods: {
+  //   increment() {
+  //     this.$store.commit('increment')
+  //   }
+  // }
+});
+</script>
